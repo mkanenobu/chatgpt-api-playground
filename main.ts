92bb3577e2fa22bb06bbc0523f2a6efa63459a0d
mkanenobu <mitsuru.kanenobu@gmail.com>
2023-11-11 12:49:23 +0900

    update

--- main.ts
+++ main.ts
@@ -1,48 +1,27 @@
-import { Configuration, OpenAIApi } from "openai";
-import { ChatCompletionRequestMessageRoleEnum } from "openai/api";
-import { z } from "zod";
-import { zodToJsonSchema } from "zod-to-json-schema";
-import { configDotenv } from "dotenv";
-
-configDotenv();
+import { OpenAI } from "openai";
+// import { z } from "zod";
+// import { zodToJsonSchema } from "zod-to-json-schema";
 
 const prettify = (obj: any) => JSON.stringify(obj, undefined, 2);
 
 const apiKey = process.env.API_KEY;
 
-const configuration = new Configuration({
+const openaiClient = new OpenAI({
   apiKey,
 });
 
-const openaiClient = new OpenAIApi(configuration);
-
-const schema = z.object({
-  questions: z.array(
-    z.object({
-      name: z.string(),
-      email: z.string().email(),
-      role: z.enum(["admin", "user"]),
-    })
-  ),
-});
-
 const main = async () => {
-  const jsonSchema = zodToJsonSchema(schema);
-  console.log("jsonSchema", JSON.stringify(jsonSchema, undefined, 2));
+  // const jsonSchema = zodToJsonSchema(schema);
+  // console.log("jsonSchema", JSON.stringify(jsonSchema, undefined, 2));
 
-  const res = await openaiClient
-    .createChatCompletion({
-      model: "gpt-3.5-turbo-0613",
+  const res = await openaiClient.chat.completions
+    .create({
+      max_tokens: 500,
+      model: "gpt-4-vision-preview",
       messages: [
         {
-          role: ChatCompletionRequestMessageRoleEnum.User,
-          content: "ダミーのデータを5人分生成してください",
-        },
-      ],
-      functions: [
-        {
-          name: "createUserData",
-          parameters: jsonSchema,
+          role: "user",
+          content: "ダミーのアンケートへの回答データを5人分生成してください",
         },
       ],
     })
@@ -51,11 +30,7 @@ const main = async () => {
       return undefined;
     });
 
-  const functionCall = JSON.parse(
-    res?.data.choices.at(0)?.message?.function_call?.arguments
-  );
-
-  console.log(prettify(functionCall));
+  console.log(prettify(res));
   // console.log(prettify(schema.safeParse(functionCall)));
 };
 
